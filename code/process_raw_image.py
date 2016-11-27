638075c58f0d1ea5a68b2921785c96043f4a570e
Joon Hyuck Choi <joonhyuck.choi92@gmail.com>
2016-11-27 15:44:45 -0500

    Add doc2.png and doc2_thresh.png

--- code/process_raw_image.py
+++ code/process_raw_image.py
@@ -5,9 +5,9 @@ import numpy as np
 Code adapted from docs.opencv.org/trunk/d7/d4d/tutorial_py_thresholding.html
 """
 
-img = cv2.imread('ad1.png', 0)
+img = cv2.imread('doc2.png', 0)
 img = cv2.medianBlur(img, 5)
 
 th = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
 
-cv2.imwrite('ad1_thresh.png', th)
+cv2.imwrite('doc2_thresh.png', th)
